aa9f88185e3373c4ba54331e67a3746daa787d35
DAVID PARRA MEDINA <davidparramedina@hotmail.com>
2024-07-30 20:55:34 -0500

    agrege paginas hombre y mujer y componente carrusel

--- frontEnd/src/app/pages/hombre/hombre.component.ts
+++ frontEnd/src/app/pages/hombre/hombre.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../component/nav/nav.component';
 import { FooterComponent } from '../../component/footer/footer.component';
+import { CarruselComponent } from "../../component/carrusel/carrusel.component";
 
 @Component({
   selector: 'app-hombre',
   standalone: true,
-  imports: [NavComponent, FooterComponent],
+  imports: [NavComponent, FooterComponent, CarruselComponent],
   templateUrl: './hombre.component.html',
   styleUrl: './hombre.component.css'
 })
 export class HombreComponent {
-
 }
