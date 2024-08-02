c0ee75ed2ef1b18fea27823a302a64039f583470
beltracr <cristiabeltran0628@gamil.com>
2024-08-02 07:15:03 -0500

    actualizacion modelo admin acorde clase de la profe

--- backend/models/admin.Model.js
+++ backend/models/admin.Model.js
@@ -1,28 +1,16 @@
 import mongoose from "mongoose";
+import { usuariosModel } from "./users.Model.js";
 
 
 const schema = mongoose.Schema;
 const adminSchema = new schema({
 
-    nombre:{
-        type: String,
-        required: true
-    },
-
-    correo:{
-        type: String,
-        required: true,
-        unique: true
-    
-    },
-    contrasena:{
-        type: String,
-        required: true
-    },
+  
 
     CategoriaAdmin:{
 
         type: Boolean,
+        required: true,
         default: true
     }
 
@@ -30,4 +18,4 @@ const adminSchema = new schema({
 
 });
 
-export const adminModel = mongoose.model("administradores",adminSchema);
+export const adminModel = usuariosModel.discriminator("admin",adminSchema)
