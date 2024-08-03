e26090c8ee4ec4f4e1518da4c99d60f0b09e65a4
Angelo Gonzalez <angellog9714@gmail.com>
2024-08-02 19:11:08 -0500

    cambios admin model

--- backend/models/admin.Model.js
+++ backend/models/admin.Model.js
@@ -5,26 +5,21 @@ import { usuariosModel } from "./users.Model.js";
 const schema = mongoose.Schema;
 const adminSchema = new schema({
 
-<<<<<<< HEAD
-  
-=======
-    nombre:{
-        type: String,
-        required: true
-    },
-    correo:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    contrasena:{
-        type: String,
-        required: true
-    },
->>>>>>> angeloGonzalez
+    // nombre:{
+    //     type: String,
+    //     required: true
+    // },
+    // correo:{
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    // contrasena:{
+    //     type: String,
+    //     required: true
+    // },
 
     CategoriaAdmin:{
-
         type: Boolean,
         required: true,
         default: true
