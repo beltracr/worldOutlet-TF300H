b2619356db35ba40e0454469089f1c372f3507b9
DAVID PARRA MEDINA <davidparramedina@hotmail.com>
2024-08-24 22:21:52 -0500

    actualizacion vistas administrador

---
+++ frontEnd/src/app/administrador/components/sidebar/sidebar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
+import {RouterLink, RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    NgClass,
+    RouterLink,
+    RouterOutlet,
+    NgIf
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  isCollapsed = true;
+
+  expandSidebar() {
+    this.isCollapsed = false;
+  }
+
+  collapseSidebar() {
+    this.isCollapsed = true;
+  }
+}
