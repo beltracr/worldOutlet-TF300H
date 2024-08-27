08ccef0fa4f902fb6f755b62f282e139a34ee501
beltracr <cristiabeltran0628@gamil.com>
2024-08-27 08:31:01 -0500

    actualizacion

--- frontEnd/src/app/administrador/pages/productos/productos.component.ts
+++ frontEnd/src/app/administrador/pages/productos/productos.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import { NgForOf } from "@angular/common";
 import { ProductsService } from '../../../services/products.service';
 import { FormsModule } from '@angular/forms';
 
@@ -9,17 +9,17 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-productos',
   standalone: true,
   imports: [
-    NgForOf,FormsModule
+    NgForOf, FormsModule
   ],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
 //export class ProductosComponent {
- // productos = [
-   // { id: 1, categoria: 'Hombre', nombre: 'Nike AirForze', cantidad: 50, imagen: "assets/images/logo.png" },
-   //{ id: 2, categoria: 'Mujer', nombre: 'Blusa Nike', cantidad: 30, imagen: "assets/images/logo.png" },
-    // Más productos...
-  //];
+// productos = [
+// { id: 1, categoria: 'Hombre', nombre: 'Nike AirForze', cantidad: 50, imagen: "assets/images/logo.png" },
+//{ id: 2, categoria: 'Mujer', nombre: 'Blusa Nike', cantidad: 30, imagen: "assets/images/logo.png" },
+// Más productos...
+//];
 //}
 
 export class ProductosComponent {
@@ -27,70 +27,87 @@ export class ProductosComponent {
   producto = inject(ProductsService)
 
 
-  todosProductos: any[]=[]
+  todosProductos: any[] = []
   mostrarFormulario: boolean = false;
-  id:string = " ";
-  nombre:string = " ";
-  imagen:string = " ";
-  descripcion:string = " ";
-  talla:[string] = [""];
-  color: [string]= [""];
+  id: string = " ";
+  nombre: string = " ";
+  imagen: string = " ";
+  descripcion: string = " ";
+  talla: [string] = [""];
+  color: [string] = [""];
   categoria: string = " ";
-  cantidad:number = 0;
-  precio:number = 0;
+  cantidad: number = 0;
+  precio: number = 0;
   editMode: boolean = false;
   editarProductoId: string | null = null;
 
 
-  obtenerProductos(){
-    this.producto.getProducts().subscribe((res: any) =>{
-      if (res){
-        console.log("res",res);
-        this.todosProductos = res
-      } else {
-        console.error('hubo un error');
+  obtenerProductos() {
+    this.producto.getProducts().subscribe((res: any) => {
+      try {
+        if (res) {
+          console.log("res", res);
+          this.todosProductos = res
+        }
+      } catch (error) { console.log(error) }
+    })
+
+  }
+
+  borrarProducto(id: string) {
+
+  }
+
+  crearProductos() {
+    if(!this.nombre || !this.imagen || !this.descripcion|| 
+      !this.talla || !this.color || !this.categoria || 
+      !this.cantidad || !this.precio ){
+        console.log("se debe ingresar todos los campos")
+      }else{
+        console.log(this.nombre, this.imagen, this.descripcion,
+           this.talla, this.color, this.categoria,
+           this.cantidad, this.precio);
+           this.producto.postProducts(this).subscribe((res:any)=>{
+            try{
+              if(res){
+                console.log("res",res)
+              }
+            }catch(error){console.log(error)}
+           })
       }
-      })
+   
+
+  }
+  editarProductos() {
 
   }
 
-  borrarProducto(id:string){
-
-  }
-
-  crearProductos(){
-
-  }
- editarProductos(){
-
-  }
-
-  identificarId(id:string) {
+  identificarId(id: string) {
     this.editarProductoId = id;
-     this.editMode = true;
-     this.mostrarFormulario = true;
-     console.log(this.editarProductoId);
-   }
+    this.editMode = true;
+    this.mostrarFormulario = true;
+    console.log(this.editarProductoId);
+  }
 
 
 
-  ngOnInit(){
+  ngOnInit() {
     this.obtenerProductos()
   }
   toggleDiv() {
     this.mostrarFormulario = !this.mostrarFormulario;
-  if (!this.mostrarFormulario) {
-  this.nombre = " ";
-  this.imagen = " ";
-  this.descripcion = " ";
-  this.talla = [""];
-  this.color = [""];
-  this.categoria = " ";
-  this.cantidad = 0;
-  this.precio = 0;
-  this.editMode = false;
-  this.editarProductoId = null;
-  }
+    if (!this.mostrarFormulario) {
+      this.nombre = " ";
+      this.imagen = " ";
+      this.descripcion = " ";
+      this.talla = [""];
+      this.color = [""];
+      this.categoria = " ";
+      this.cantidad = 0;
+      this.precio = 0;
+      this.editMode = false;
+      this.editarProductoId = null;
+    }
   }
 
 
