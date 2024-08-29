b87c28b6393c9e920cb2d0e316e5f9fbf55272cc
beltracr <cristiabeltran0628@gamil.com>
2024-08-28 21:33:53 -0500

    actualizacion

--- frontEnd/src/app/administrador/pages/productos/productos.component.ts
+++ frontEnd/src/app/administrador/pages/productos/productos.component.ts
@@ -8,6 +8,8 @@ import { HttpHeaders } from '@angular/common/http';
 import { ThisReceiver } from '@angular/compiler';
 import { Products } from '../../../interfaces/products';
 import Swal from 'sweetalert2';
+import { RouterLink } from '@angular/router';
+
 
 
 
@@ -24,7 +26,7 @@ import Swal from 'sweetalert2';
   selector: 'app-productos',
   standalone: true,
   imports: [
-    NgForOf, FormsModule, 
+    NgForOf, FormsModule,
   ],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
@@ -61,6 +63,15 @@ export class ProductosComponent {
   editarProductoId: string | null = null;
 
 
+ todosLosCampos(){
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "se debe ingresar todos los campos",
+    footer: '<a href="/admin/inventario">¿Quieres ir atras?</a>'
+  });
+ }
+
 
 
   obtenerProductos() {
@@ -74,54 +85,75 @@ export class ProductosComponent {
     })
 
   }
+// 
+// 
+// 
+// 
+// borrar
+
+botonBorrar(id:string){
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger"
+    },
+    buttonsStyling: false
+  });
+  swalWithBootstrapButtons.fire({
+    title: "¿Estas seguro de eliminar el producto seleccionado?",
+    text: "¡No podras revertirlo!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Si, eliminar",
+    cancelButtonText: "No, cancelar!",
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+      
+      this.borrarProducto(id)
+      swalWithBootstrapButtons.fire({
+        title: "Enhorabuena",
+        text: "El producto ha sido editado",
+        icon: "success"
+      });
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire({
+        title: "cancelado",
+        text: "Tu producto no se elimino",
+        icon: "error"
+      });
+    }
+  });
+
+}
+
 
 
 
   borrarProducto(id: string) {
-    console.log("producto que se borrara tiene el id =", id);
-    let isConfirm: boolean = false
-    isConfirm = confirm("¿Esta seguro que quiere eliminar este producto?");
-    if (isConfirm) {
+
       this.producto.deleteProducts(id).subscribe((res: any) => {
         try {
           if (res) {
             console.log("res", res);
-            alert("Se elimino correctamente")
+            console.log("Se elimino correctamente")
             this.obtenerProductos()
           }
         } catch (error) { console.log(error) }
       })
     }
-  }
-  // Swal.fire({
-  //   title: "Are you sure?",
-  //   text: "You won't be able to revert this!",
-  //   icon: "warning",
-  //   showCancelButton: true,
-  //   confirmButtonColor: "#3085d6",
-  //   cancelButtonColor: "#d33",
-  //   confirmButtonText: "Yes, delete it!"
-  // }).then((result) => {
-  //   if (result.isConfirmed) {
-  //     Swal.fire({
-  //       title: "Deleted!",
-  //       text: "Your file has been deleted.",
-  //       icon: "success"
-  //     });
-  //   }
-  // });
-
-
-
-
-
-
-
-
-
-
-
-  // logica para crear producto
+  
+// 
+// 
+// 
+// 
+// 
+// 
+//   Crear 
 
   crearProductos() {
 
@@ -129,6 +161,8 @@ export class ProductosComponent {
       !this.talla || !this.color || !this.categoria ||
       !this.cantidad || !this.precio) {
       console.log("se debe ingresar todos los campos")
+      this.errorCrear()
+      
     } else {
 
       const nuevoProducto: Products = {
@@ -145,11 +179,51 @@ export class ProductosComponent {
         try {
           if (res) {
             console.log("res", res)
+            this.obtenerProductos()
+            this.notificacionCrear()
           }
         } catch (error) { alert(error) }
       })
     }
   }
+
+notificacionCrear(){
+  
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 1500
+  });
+}
+
+errorCrear(){
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Debes ingresar todos los valores",
+    footer: '<a href="/admin">¿quieres volver atras?</a>'
+  });
+}
+
+
+
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//editar:
+
+
+
 
   identificarId(id: string) {
     this.editarProductoId = id;
@@ -158,12 +232,60 @@ export class ProductosComponent {
     console.log(this.editarProductoId);
   }
 
-  editarProductos() {
+  botonEditar() {
 
     if (!this.nombre || !this.imagen || !this.descripcion ||
       !this.talla || !this.color || !this.categoria ||
       !this.cantidad || !this.precio) {
       console.log("se debe ingresar todos los campos")
+      this.todosLosCampos()
+      
+    } else {
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "¿Estas seguro de editar el producto seleccionado?",
+      text: "¡No podras revertirlo!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, editar",
+      cancelButtonText: "No, cancelar!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.editarProductos()
+        swalWithBootstrapButtons.fire({
+          title: "Enhorabuena",
+          text: "El producto ha sido editado",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "cancelado",
+          text: "Tu producto sigue igual",
+          icon: "error"
+        });
+      }
+    });
+  }}
+
+  editarProductos() {
+
+
+    if (!this.nombre || !this.imagen || !this.descripcion ||
+      !this.talla || !this.color || !this.categoria ||
+      !this.cantidad || !this.precio) {
+      alert("se debe ingresar todos los campos")
+      
     } else {
       try {
 
@@ -177,31 +299,29 @@ export class ProductosComponent {
           cantidad: this.cantidad,
           precio: this.precio
         }
-
         if (this.editarProductoId) {
-          let isConfirm: boolean = false
-          isConfirm = confirm("¿Esta seguro que quiere editar este producto?");
-          if (isConfirm){
-            this.producto.updateProducts(productoActualizado, this.editarProductoId).subscribe((res: any) => {
-              if (res) {
-                console.log("res", res);
-                alert("se elimino correctamente")
-                this.obtenerProductos()
+          this.producto.updateProducts(productoActualizado, this.editarProductoId).subscribe((res: any) => {
+            if (res) {
+              console.log("res", res);
+              console.log("se elimino correctamente")
+              this.obtenerProductos()
 
-              }
-            })
-        }}
+            }
+          })
+        }
       } catch (error) {
         console.log(error)
-
       }
 
     }
 
   }
-
-
-
+// 
+// 
+// 
+// 
+// 
+// 
 
 
   ngOnInit() {
