36beecdc1ab8f3819618e1361c233c427e718ef1
beltracr <cristiabeltran0628@gamil.com>
2024-08-30 18:49:54 -0500

    actualizacion

--- frontEnd/src/app/component/nav/nav.component.ts
+++ frontEnd/src/app/component/nav/nav.component.ts
@@ -1,15 +1,59 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ModalIniSesionComponent } from '../modal-ini-sesion/modal-ini-sesion.component';
+import { Input } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
+import { Credentials } from '../../interfaces/credentials';
+import { UserloginServiceService } from '../../services/userlogin.service.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [ RouterLink,ModalIniSesionComponent],
+  imports: [ RouterLink,ModalIniSesionComponent,ReactiveFormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 
- 
+  loginService = inject(UserloginServiceService);
+  @Input() toggleRegister: () => void = () => {};
+  credentialForm = new FormGroup({
+    correo: new FormControl(''),
+    contrasena: new FormControl('')
+  });
+
+  getCredentials(){
+    const correo = this.credentialForm.value.correo;
+    const contrasena = this.credentialForm.value.contrasena;
+    if(typeof correo === 'string' && typeof contrasena === 'string'){
+      const validarCredenciales: Credentials = {
+        correo,
+        contrasena
+      }
+      return validarCredenciales;
+    }
+    return null;
+  }
+  handleSubmint(){
+    const credenciales = this.getCredentials();
+    console.log(credenciales);
+    if(credenciales){
+      this.loginService.login(credenciales).subscribe({
+        next: (res: any) => {
+          console.log(res)
+          if(res){
+            localStorage.setItem('token', res.tokenGenerado)
+            this.loginService.redirect();
+          }
+
+        },
+        error: (err)=>{
+          alert(err.error.mensaje);
+          this.credentialForm.reset();
+        }
+      })
+    }
+
+
+  }
 }
