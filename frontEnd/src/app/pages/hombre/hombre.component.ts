9285af08710f5e0fef5a49398aea60a1bf201579
Angelo Gonzalez <angellog9714@gmail.com>
2024-09-02 16:28:57 -0500

    modales de información en pagina hombre mujer

--- frontEnd/src/app/pages/hombre/hombre.component.ts
+++ frontEnd/src/app/pages/hombre/hombre.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, ElementRef } from '@angular/core';
 import { NavComponent } from '../../component/nav/nav.component';
 import { FooterComponent } from '../../component/footer/footer.component';
 import { CarruselComponent } from "../../component/carrusel/carrusel.component";
@@ -19,18 +19,9 @@ export class HombreComponent {
   producto = inject(ProductsService);
   
   todosProductos: any[] = []
- 
-  
- 
-
 
   obtenerProductos() {
-
-   
-
-
-  
-    this.producto.getProducts().subscribe((res: any) => {
+this.producto.getProducts().subscribe((res: any) => {
       try {
         if (res) {
           console.log("res", res);
@@ -38,14 +29,36 @@ export class HombreComponent {
         }
       } catch (error) { console.log(error) }
     })
-  
-
   }
-  
-
 
   ngOnInit() {
     this.obtenerProductos()
+  }
+
+  
+  // Logica boton scroll activado
+
+
+  // scrol para la section de empoderate 
+  @ViewChild('correr', { static: true }) correr!: ElementRef;
+
+  runLeft (){
+    this.correr.nativeElement.scrollLeft -= 400
+  }
+
+  runRigh (){
+    this.correr.nativeElement.scrollLeft += 400
+  }
+
+  // scrol para la section de deportes
+  @ViewChild('deportes', { static: true }) deportes!: ElementRef;
+
+  LeftRn (){
+    this.deportes.nativeElement.scrollLeft -= 400
+  }
+
+  RighRn (){
+    this.deportes.nativeElement.scrollLeft += 400
   }
 
 
